9ad4749426bc7f70e310b1d2a324569e919f3075
agusev <audetv@gmail.com>
2023-01-30 11:28:36 +0300

    Added DI configuration

--- api/public/index.php
+++ api/public/index.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use DI\Container;
+use DI\ContainerBuilder;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
@@ -13,17 +14,31 @@ http_response_code(500);
 require __DIR__ . '/../vendor/autoload.php';
 
 /**
- * Создаем экземпляр контейнера
+ * Создаем экземпляр билдера контейнера и передаем определения для построения контейнера
  */
-$container = new Container();
+$builder = new ContainerBuilder();
+
+$builder->addDefinitions(
+    [
+        'config' => [
+            'debug' => (bool)getenv('APP_DEBUG'),
+        ],
+    ]);
+/**
+ * Создаем экземпляр контейнера с помощью билдера.
+ * @var Container $container
+ */
+$container = $builder->build();
 
 /**
  * Вызываем фабрику createFromContainer и передаем туда экземпляр контейнера
  * ContainerInterface стандарта psr7
  */
 $app = AppFactory::createFromContainer($container);
-
-$app->addErrorMiddleware((bool)getenv('APP_DEBUG'), true, true);
+/**
+ * Передаем значение параметра displayErrorDetails из контейнера
+ */
+$app->addErrorMiddleware($container->get('config')['debug'], true, true);
 
 $app->get('/', function (Request $request, Response $response, $args) {
     throw new \Slim\Exception\HttpNotFoundException($request);
