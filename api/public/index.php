bbe1f3142ce70e1c51b0aedabf58f1c4ef8f9629
agusev <audetv@gmail.com>
2023-01-30 09:52:04 +0300

    Installed Slim

--- api/public/index.php
+++ api/public/index.php
@@ -2,6 +2,20 @@
 
 declare(strict_types=1);
 
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\Factory\AppFactory;
+
+
 header('Content-Type: application/json');
 
-echo '{}';
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->get('/', function (ServerRequestInterface $request, ResponseInterface $response, $args) {
+    $response->getBody()->write('{}');
+    return $response->withHeader('Content-Type', 'application/json');
+});
+
+$app->run();
