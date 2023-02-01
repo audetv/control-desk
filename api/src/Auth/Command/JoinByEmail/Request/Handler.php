7c10bcc430783165c9ad92e407a78496bb4c0ee3
agusev <audetv@gmail.com>
2023-02-01 17:57:01 +0300

    Draft Join by email request handler

--- api/src/Auth/Command/JoinByEmail/Request/Handler.php
+++ api/src/Auth/Command/JoinByEmail/Request/Handler.php
@@ -4,10 +4,35 @@ declare(strict_types=1);
 
 namespace App\Auth\Command\JoinByEmail\Request;
 
+use App\Auth\Entity\User\Email;
+use App\Auth\Entity\User\Id;
+use DateTimeImmutable;
+use DomainException;
+
 class Handler
 {
     public function handle(Command $command): void
     {
-        
+        $email = new Email($command->email);
+
+        if ($this->users->hasByEmail($email)) {
+            throw new DomainException('User already exist.');
+        }
+
+        $date = new DateTimeImmutable();
+
+        $user = new User(
+            Id::generate(),
+            $date,
+            $email,
+            $this->hasher->hash($command->password),
+            $token = $this->tokenizer->generate($date)
+        );
+
+        $this->users->add($user);
+
+        $this->flusher->flush();
+
+        $this->sender->send($email, $token);
     }
 }
