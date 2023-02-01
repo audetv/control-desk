0396254a32f3a1974977611a240a52c38381c15b
Aleksey Gusev <audetv@gmail.com>
2023-02-01 22:11:54 +0300

    Added password hasher

---
+++ api/src/Auth/Test/Unit/Service/PasswordHasherTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Auth\Test\Unit\Service;
+
+use App\Auth\Service\PasswordHasher;
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+
+class PasswordHasherTest extends TestCase
+{
+    public function testHash(): void
+    {
+        $hasher = new PasswordHasher();
+
+        $hash = $hasher->hash($password = 'password');
+
+        self::assertNotEmpty($hash);
+        self::assertNotEquals($password, $hash);
+    }
+
+    public function testHashEmpty(): void
+    {
+        $hasher = new PasswordHasher();
+
+        $this->expectException(InvalidArgumentException::class);
+        $hasher->hash('');
+    }
+
+    public function testValidate(): void
+    {
+        $hasher = new PasswordHasher();
+        $hash = $hasher->hash($password = 'password');
+
+        self::assertTrue($hasher->validate($password, $hash));
+        self::assertFalse($hasher->validate('wrong-password', $hash));
+    }
+}
