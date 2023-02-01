c4431311c91fb0b370783a909d344cd7f546c472
Aleksey Gusev <audetv@gmail.com>
2023-02-01 20:19:13 +0300

    Added join request use case without tests

---
+++ api/src/Auth/Entity/User/User.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Auth\Entity\User;
+
+use DateTimeImmutable;
+
+class User
+{
+    private Id $id;
+    private DateTimeImmutable $date;
+    private Email $email;
+    private string $passwordHash;
+    private ?Token $joinConfirmToken;
+
+    public function __construct(
+        Id $id,
+        DateTimeImmutable $date,
+        Email $email,
+        string $passwordHash,
+        Token $token
+    ) {
+        $this->id = $id;
+        $this->date = $date;
+        $this->email = $email;
+        $this->passwordHash = $passwordHash;
+        $this->joinConfirmToken = $token;
+    }
+
+    /**
+     * @return Id
+     */
+    public function getId(): Id
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return DateTimeImmutable
+     */
+    public function getDate(): DateTimeImmutable
+    {
+        return $this->date;
+    }
+
+    /**
+     * @return Email
+     */
+    public function getEmail(): Email
+    {
+        return $this->email;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getPasswordHash(): ?string
+    {
+        return $this->passwordHash;
+    }
+
+    /**
+     * @return Token|null
+     */
+    public function getJoinConfirmToken(): ?Token
+    {
+        return $this->joinConfirmToken;
+    }
+}
