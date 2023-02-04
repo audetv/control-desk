d5bb4f1d33efc8596e61de4cc1a2b847b1b53fa3
Aleksey Gusev <audetv@gmail.com>
2023-02-04 10:48:02 +0300

    Added manager configuration, not tested

---
+++ api/config/common/doctrine.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Auth;
+use Symfony\Component\Cache\Adapter\ArrayAdapter;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\UnderscoreNamingStrategy;
+use Doctrine\ORM\ORMSetup;
+use Psr\Container\ContainerInterface;
+
+return [
+    EntityManagerInterface::class => function (ContainerInterface $container): EntityManagerInterface {
+        /**
+         * @psalm-suppress MixedArrayAccess
+         * @var array{
+         *     metadata_dirs:string[],
+         *     dev_mode:bool,
+         *     proxy_dir:string,
+         *     cache_dir:?string,
+         *     types:array<string,class-string<Doctrine\DBAL\Types\Type>>,
+         *     subscribers:string[],
+         *     connection:array<string, mixed>
+         * } $settings
+         */
+        $settings = $container->get('config')['doctrine'];
+
+        $config = ORMSetup::createAttributeMetadataConfiguration(
+            $settings['metadata_dirs'],
+            $settings['dev_mode'],
+            $settings['proxy_dir'],
+            $settings['cache_dir'] ? new FilesystemAdapter('', 0, $settings['cache_dir']) : new ArrayAdapter()
+        );
+
+        $config->setNamingStrategy(new UnderscoreNamingStrategy());
+
+        return EntityManager::create(
+            $settings['connection'],
+            $config
+        );
+    },
+
+    'config' => [
+        'doctrine' => [
+            'dev_mode' => false,
+            'cache_dir' => __DIR__ . '/../../var/cache/doctrine/cache',
+            'proxy_dir' => __DIR__ . '/../../var/cache/doctrine/proxy',
+            'connection' => [
+                'driver' => 'pdo_pgsql',
+                'host' => getenv('DB_HOST'),
+                'user' => getenv('DB_USER'),
+                'password' => getenv('DB_PASSWORD'),
+                'dbname' => getenv('DB_NAME'),
+                'charset' => 'utf-8'
+            ],
+            'metadata_dirs' => [],
+        ],
+    ],
+];
