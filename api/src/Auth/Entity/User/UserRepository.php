3402d74848a1a2d44d95f8e52a7b4f166d99d3f9
Aleksey Gusev <audetv@gmail.com>
2023-02-04 19:46:51 +0300

    Implemented user repository

--- api/src/Auth/Entity/User/UserRepository.php
+++ api/src/Auth/Entity/User/UserRepository.php
@@ -4,35 +4,93 @@ declare(strict_types=1);
 
 namespace App\Auth\Entity\User;
 
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
 use DomainException;
 
-interface UserRepository
+class UserRepository
 {
-    public function hasByEmail(Email $email): bool;
+    /**
+     * @var EntityRepository<User>
+     */
+    private EntityRepository $repo;
+    private EntityManagerInterface $em;
 
-    public function add(User $user): void;
+    public function __construct(EntityManagerInterface $em)
+    {
+        $repo = $em->getRepository(User::class);
+        $this->repo = $repo;
+        $this->em = $em;
+    }
 
-    public function findByNewEmailToken(string $token): ?User;
+    public function findByJoinConfirmToken(string $token): ?User
+    {
+        return $this->repo->findOneBy(['joinConfirmToken.value' => $token]);
+    }
 
-    public function findByJoinConfirmToken(string $token): ?User;
+    public function hasByEmail(Email $email): bool
+    {
+        return $this->repo->createQueryBuilder('t')
+                ->select('COUNT(t.id)')
+                ->andWhere('t.email = :email')
+                ->setParameter(':email', $email->getValue())
+                ->getQuery()->getSingleScalarResult() > 0;
+    }
 
-    public function findByPasswordResetToken(string $token): ?User;
+    public function add(User $user): void
+    {
+        $this->em->persist($user);
+    }
 
-    public function hasByNetwork(Network $network): ?User;
+    public function findByNewEmailToken(string $token): ?User
+    {
+        return $this->repo->findOneBy(['newEmailToken.value' => $token]);
+    }
+
+    public function findByPasswordResetToken(string $token): ?User
+    {
+        return $this->repo->findOneBy(['passwordResetToken.value' => $token]);
+    }
+
+    public function hasByNetwork(Network $network): bool
+    {
+        return $this->repo->createQueryBuilder('t')
+                ->select('COUNT(t.id)')
+                ->innerJoin('t.networks', 'n')
+                ->andWhere('n.network.name = :name and n.network.identity = :identity')
+                ->setParameter(':name', $network->getName())
+                ->setParameter(':identity', $network->getIdentity())
+                ->getQuery()->getSingleScalarResult() > 0;
+    }
 
     /**
      * @param Id $id
      * @return User
      * @throws DomainException
      */
-    public function get(Id $id): User;
+    public function get(Id $id): User
+    {
+        if (!$user = $this->repo->find($id->getValue())) {
+            throw new DomainException('User is not found.');
+        }
+        return $user;
+    }
 
     /**
      * @param Email $email
      * @return User
      * @throws DomainException
      */
-    public function getByEmail(Email $email): User;
+    public function getByEmail(Email $email): User
+    {
+        if (!$user = $this->repo->findOneBy(['email' => $email->getValue()])) {
+            throw new DomainException('user id not found.');
+        }
+        return $user;
+    }
 
-    public function remove(User $user): void;
+    public function remove(User $user): void
+    {
+        $this->em->remove($user);
+    }
 }
