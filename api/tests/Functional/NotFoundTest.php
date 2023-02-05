19f4e3e8b1f05392ba9fbcd1456a76453b661464
Aleksey Gusev <audetv@gmail.com>
2023-02-05 22:11:15 +0300

    Fixed not found page test

--- api/tests/Functional/NotFoundTest.php
+++ api/tests/Functional/NotFoundTest.php
@@ -4,21 +4,24 @@ declare(strict_types=1);
 
 namespace Test\Functional;
 
-use Fig\Http\Message\RequestMethodInterface;
 use DMS\PHPUnitExtensions\ArraySubset\ArraySubsetAsserts;
+use Fig\Http\Message\RequestMethodInterface;
 
 class NotFoundTest extends WebTestCase
 {
+    use ArraySubsetAsserts;
+
     public function testNotFound(): void
     {
         $response = $this->app()->handle(self::json(RequestMethodInterface::METHOD_GET, '/not-found'));
         self::assertEquals(404, $response->getStatusCode());
         self::assertJson($body = (string)$response->getBody());
 
+        /** @var array $data */
         $data = json_decode($body, true, 512, JSON_THROW_ON_ERROR);
 
         self::assertArraySubset([
-           'message' => '404 Not Found',
-       ], $data);
+            'message' => '404 Not Found',
+        ], $data);
     }
 }
