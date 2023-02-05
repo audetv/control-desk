479f5f125c1609be608b6ad29769b3062167ca39
Aleksey Gusev <audetv@gmail.com>
2023-02-05 14:59:33 +0300

    Removed useless JoinConfirmationSender (simple class with simple constructor di container - auto)

--- api/config/common/auth.php
+++ api/config/common/auth.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 use App\Auth;
 use App\Auth\Entity\User\User;
 use App\Auth\Entity\User\UserRepository;
-use App\Auth\Service\JoinConfirmationSender;
-use App\Frontend\FrontendUrlGenerator;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Container\ContainerInterface;
 
@@ -15,12 +13,5 @@ return [
         $em = $container->get(EntityManagerInterface::class);
         $repo = $em->getRepository(User::class);
         return new UserRepository($em, $repo);
-    },
-    JoinConfirmationSender::class => function (ContainerInterface $container): JoinConfirmationSender {
-        $mailer = $container->get(Swift_Mailer::class);
-
-        $frontend = $container->get(FrontendUrlGenerator::class);
-
-        return new JoinConfirmationSender($mailer, $frontend);
-    },
+    }
 ];
