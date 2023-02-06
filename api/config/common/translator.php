0847fd163f72702876e4119cec882ec4ff4ad4a6
Aleksey Gusev <audetv@gmail.com>
2023-02-06 23:00:39 +0300

    Added validation translations

--- api/config/common/translator.php
+++ api/config/common/translator.php
@@ -3,6 +3,8 @@
 declare(strict_types=1);
 
 use Psr\Container\ContainerInterface;
+use Symfony\Component\Translation\Loader\PhpFileLoader;
+use Symfony\Component\Translation\Loader\XliffFileLoader;
 use Symfony\Component\Translation\Translator;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
@@ -14,12 +16,28 @@ return [
          */
         $config = $container->get('config')['translator'];
 
-        return new Translator($config['lang']);
+        $translator = new Translator($config['lang']);
+        $translator->addLoader('php', new PhpFileLoader());
+        $translator->addLoader('xlf', new XliffFileLoader());
+
+        foreach ($config['resources'] as $resource) {
+            $translator->addResource(...$resource);
+        }
+
+        return $translator;
     },
 
     'config' => [
         'translator' => [
             'lang' => 'en',
+            'resources' => [
+                [
+                    'xlf',
+                    __DIR__ . '/../../vendor/symfony/validator/Resources/translations/validators.ru.xlf',
+                    'ru',
+                    'validators'
+                ],
+            ],
         ],
     ],
 ];
