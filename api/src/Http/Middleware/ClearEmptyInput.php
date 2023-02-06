6da3ad8ce2877a40cfc8eaa99c93a394670be816
agusev <audetv@gmail.com>
2023-02-06 15:13:54 +0300

    Improved ClearEmptyInput middleware with filterFiles

--- api/src/Http/Middleware/ClearEmptyInput.php
+++ api/src/Http/Middleware/ClearEmptyInput.php
@@ -6,6 +6,7 @@ namespace App\Http\Middleware;
 
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\UploadedFileInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 
@@ -14,7 +15,8 @@ class ClearEmptyInput implements MiddlewareInterface
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         $request = $request
-            ->withParsedBody(self::filterString($request->getParsedBody()));
+            ->withParsedBody(self::filterString($request->getParsedBody()))
+            ->withUploadedFiles(self::filterFiles($request->getUploadedFiles()));
 
         return $handler->handle($request);
     }
@@ -40,6 +42,27 @@ class ClearEmptyInput implements MiddlewareInterface
                 $result[$key] = trim($item);
             } else {
                 $result[$key] = self::filterString($item);
+            }
+        }
+
+        return $result;
+    }
+
+    private static function filterFiles(array $items): array
+    {
+        $result = [];
+
+        /**
+         * @var string $key
+         * @var array|UploadedFileInterface $item
+         */
+        foreach ($items as $key => $item) {
+            if ($item instanceof UploadedFileInterface) {
+                if ($item->getError() !== UPLOAD_ERR_NO_FILE) {
+                    $result[$key] = $item;
+                }
+            } else {
+                $result[$key] = self::filterFiles($item);
             }
         }
 
--- api/src/Http/Test/Unit/Middleware/ClearEmptyInputTest.php
+++ api/src/Http/Test/Unit/Middleware/ClearEmptyInputTest.php
@@ -9,8 +9,11 @@ use PHPUnit\Framework\TestCase;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use Slim\Psr7\Factory\RequestFactory;
 use Slim\Psr7\Factory\ResponseFactory;
 use Slim\Psr7\Factory\ServerRequestFactory;
+use Slim\Psr7\Factory\StreamFactory;
+use Slim\Psr7\Factory\UploadedFileFactory;
 
 class ClearEmptyInputTest extends TestCase
 {
@@ -48,6 +51,44 @@ class ClearEmptyInputTest extends TestCase
                     ],
                     $request->getParsedBody()
                 );
+                return (new ResponseFactory())->createResponse();
+            });
+
+        $middleware->process($request, $handler);
+    }
+
+    public function testUploadedFiles(): void
+    {
+        $middleware = new ClearEmptyInput();
+
+        $realFile = (new UploadedFileFactory())->createUploadedFile(
+            (new StreamFactory())->createStream(''),
+            0,
+            UPLOAD_ERR_OK,
+        );
+
+        $noFile = (new UploadedFileFactory())->createUploadedFile(
+            (new StreamFactory())->createStream(''),
+            0,
+            UPLOAD_ERR_NO_FILE,
+        );
+
+        $request = (new ServerRequestFactory())->createServerRequest('POST', 'http://test')
+            ->withUploadedFiles(
+                [
+                    'real_file' => $realFile,
+                    'none_file' => $noFile,
+                    'files' => [$realFile, $noFile]
+                ]
+            );
+
+        $handler = $this->createMock(RequestHandlerInterface::class);
+        $handler->expects($this->once())->method('handle')
+            ->willReturnCallback(static function (ServerRequestInterface $request) use ($realFile): ResponseInterface {
+                self::assertEquals([
+                    'real_file' => $realFile,
+                    'files' => [$realFile],
+                ], $request->getUploadedFiles());
                 return (new ResponseFactory())->createResponse();
             });
 
