7e37ae4ad2f55bdf1f90cd5753fb1984bca5273d
Aleksey Gusev <audetv@gmail.com>
2023-02-06 21:04:35 +0300

    Added validation rules

--- api/src/Auth/Command/JoinByEmail/Request/Command.php
+++ api/src/Auth/Command/JoinByEmail/Request/Command.php
@@ -4,8 +4,14 @@ declare(strict_types=1);
 
 namespace App\Auth\Command\JoinByEmail\Request;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 class Command
 {
+    #[Assert\NotBlank]
+    #[Assert\Email]
     public string $email = '';
+    #[Assert\Length(min: 6)]
+    #[Assert\NotBlank]
     public string $password = '';
 }
