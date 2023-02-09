4a9b307756ab8c0bd6d2e94ff821f511e2eb02b1
Aleksey Gusev <audetv@gmail.com>
2023-02-09 23:55:23 +0300

    Started shallow react testing

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
 import App from './App';
+import { createRenderer } from 'react-dom/test-utils';
+import * as ShallowRenderer from 'react-test-renderer/shallow';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const h1Element = screen.getByText(/Control Desk/i);
-  expect(h1Element).toBeInTheDocument();
+  ShallowRenderer.render(<App />)
+
+  // const h1Element = screen.getByText(/Control Desk/i);
+  // expect(h1Element).toBeInTheDocument();
 });
