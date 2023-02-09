44a43aae63f2ab31141cd1baeb794c39202a2b78
Aleksey Gusev <audetv@gmail.com>
2023-02-09 23:06:20 +0300

    Extracted welcome component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
+import Welcome from './components/Welcome';
 
 function App() {
   return (
     <div className="app">
-      <div className="welcome">
-        <h1>Control Desk</h1>
-        <p>We will be here soon</p>
-      </div>
+     <Welcome />
     </div>
   );
 }
---
+++ frontend/src/components/Welcome.js
@@ -0,0 +1,12 @@
+import './Welcome.css';
+
+function Welcome() {
+  return (
+    <div className="welcome">
+      <h1>Control Desk</h1>
+      <p>We will be here soon</p>
+    </div>
+  );
+}
+
+export default Welcome;
