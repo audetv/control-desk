0f815fb1e3b5a5eaf69881a0545704c8f205fc98
agusev <audetv@gmail.com>
2023-02-10 10:37:14 +0300

    Added shallow testing

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,10 +1,6 @@
 import App from './App';
-import { createRenderer } from 'react-dom/test-utils';
-import * as ShallowRenderer from 'react-test-renderer/shallow';
+import { shallow } from 'enzyme';
 
 test('renders learn react link', () => {
-  ShallowRenderer.render(<App />)
-
-  // const h1Element = screen.getByText(/Control Desk/i);
-  // expect(h1Element).toBeInTheDocument();
+  shallow(<App />)
 });
